8fd8496c78936faeab20511f32b2c4ccd8b54268
Edsson Madrigal <akairu@hotmail.com>
2024-04-07 21:12:34 -0600

    open ai chat

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
-import os
+from dotenv import load_dotenv
+from langchain.chains import LLMChain
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 
-if __name__ == '__main__':
-    print('Hello, World!')
-    print(os.environ['OPENAI_API_KEY'])
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Hello LangChain")
+    information = """
+        Alan Mathison Turing OBE FRS (/ˈtjʊərɪŋ/; 23 June 1912 – 7 June 1954) was an English mathematician, computer scientist, logician, cryptanalyst, philosopher and theoretical biologist.[5] Turing was highly influential in the development of theoretical computer science, providing a formalisation of the concepts of algorithm and computation with the Turing machine, which can be considered a model of a general-purpose computer.[6][7][8] He is widely considered to be the father of theoretical computer science and artificial intelligence.[9]
+
+Born in Maida Vale, London, Turing was raised in southern England. He graduated from King's College, Cambridge, with a degree in mathematics. Whilst he was a fellow at Cambridge, he published a proof demonstrating that some purely mathematical yes–no questions can never be answered by computation. He defined a Turing machine and proved that the halting problem for Turing machines is undecidable. In 1938, he earned his PhD from the Department of Mathematics at Princeton University.
+
+    """
+
+    summary_template = """
+    given the information {information} about a person I want you to create:
+    1. A short summary 2 senteces
+    2. two short interesting facts about them in a bullet point
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    res = chain.invoke(input={"information": information})
+
+    # print type of res variable
+    print(res["text"])
